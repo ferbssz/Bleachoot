11a27302c852416b48c5e7b8d5ebcf2082d56eeb
ferbssz <ferbs7710@gmail.com>
2025-06-03 06:28:07 -0300

    Mudança no script do banco

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -26,10 +26,21 @@ CREATE TABLE feedback_usuario (
     assunto VARCHAR(100) NULL,
     mensagem TEXT NOT NULL,
     dataEnvio DATETIME DEFAULT CURRENT_TIMESTAMP,
-    statusFeedback ENUM('Pendente', 'Em Análise', 'Resolvido', 'Ignorado') DEFAULT 'Pendente',
+    statusFeedback VARCHAR(20) DEFAULT 'Pendente',
+    CONSTRAINT chk_statusFeedback CHECK (statusFeedback IN ('Pendente', 'Em Análise', 'Resolvido', 'Ignorado')),
     FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
 );
 
+
+
+INSERT INTO resultado_quiz (idUsuario, pontuacao, totalQuestoes, dataRegistro) VALUES
+(1, 7, 10, '2025-05-10 10:00:00'),
+(1, 9, 10, '2025-05-15 14:30:00'),
+(1, 6, 10, '2025-05-20 09:15:00'),
+(1, 8, 10, '2025-05-25 11:00:00'),
+(1, 10, 10, '2025-06-03 16:45:00');
+
+SELECT * FROM resultado_quiz WHERE idUsuario = 1 ORDER BY dataRegistro ASC;
 SELECT * FROM usuario;
 select * from resultado_quiz;
 select * from feedback_usuario;
